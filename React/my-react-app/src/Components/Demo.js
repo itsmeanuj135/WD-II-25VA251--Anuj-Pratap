27776887fddcbc8d7fe4b0a38b508872494c5d3b
anujpratap <itsmeanuj135@gmail.com>
2026-06-18 11:59:35 +0530

    React Finish

---
+++ React/my-react-app/src/Components/Demo.js
@@ -0,0 +1,6 @@
+<div style={{ backgroundColor: "#CCC", border: "2px solid #000" }}>
+  <h1>Student Details</h1>
+  <Props name="Vikas" rollno="25B012345" marks="78" />
+  <Props name="Vikas1" rollno="25B012345" marks="56" />
+  <Props name="Vikas2" rollno="25B012345" marks="87" />
+</div>
